46d6f589bef82afd5adf4d915dc716663ddb0527
Hy <krishu@foxmail.com>
2019-05-13 01:04:23 +0800

    ss

--- src/com/arckz/location/PushService.java
+++ src/com/arckz/location/PushService.java
@@ -42,6 +42,7 @@ public class PushService extends HttpServlet {
     private String locName = "I'm here"; //坐标点上显示的名称
     private String conName = ""; //内容显示的名称
     private Boolean isBacked = false;//当客户端返回结果时用于退出循环的标志
+    private static int PUSH_NUM = 0; //推送次数
 
     private String site = "";
 
@@ -96,7 +97,8 @@ public class PushService extends HttpServlet {
             request.setAttribute("org.apache.catalina.ASYNC_SUPPORTED", true);
             //下发定位命令
             //XingeApp带标签皆为Android设备推送
-            System.out.println(XingeApp.pushTokenAndroid(000, "secretKey", "test", "测试", "token")); //推送给单个设备
+            PUSH_NUM+=1;
+            System.out.println(XingeApp.pushTokenAndroid(2100333325, "9f839394fe286525c26e3450b95cbdae", "定位", "第"+PUSH_NUM+"次", "411d9e11e7d4381e36739b65963e26c30578580e")); //推送给单个设备
 //            System.out.println(XingeApp.pushAccountAndroid(000, "myKey", "标题", "大家好!", "nickName")); //推送给单个账号
 //            System.out.println(XingeApp.pushAllAndroid(000, "myKey", "标题", "大家好!")); //推送给所有设备
 //            System.out.println(XingeApp.pushTagAndroid(000, "myKey", "标题", "大家好!", "beijing")); //推送给标签选中设备
@@ -106,22 +108,30 @@ public class PushService extends HttpServlet {
             AsyncHandler asyncHandler = new AsyncHandler(aCtx);
             userExecutor.execute(asyncHandler);
 
-        } else if (back.equals(Constants.STATIC_CLIENT_TOKEN)) {
-            if (UtilHelper.IsEmpty(token)) {
-                out.println("参数错误");
+        } else if (back.equals("1")) { //客户端请求带参数的时候
+            if (UtilHelper.IsEmpty(token)) { //和客户端验证静态token
+                out.println("Client empty token ..");
                 out.flush();
                 out.close();
-            } else {
+            } else if (token.equals(Constants.STATIC_CLIENT_TOKEN)){
                 if (!UtilHelper.IsEmpty(Lng) && !UtilHelper.IsEmpty(Lat)) {
                     isBacked = true; //放行线程
-
+                    if (locName.equals("")){
+                        locName = "老公的位置";
+                    }
+                    if (conName.equals("")){
+                        conName = "我不见咯";
+                    }
                     site = Url.MAP_BASE_URL + "marker?location=" + Lng + "," + Lat + "&title=" + locName + "&content=" + conName + "&output=html&src=webapp.baidu.openAPIdemo";
-
                 } else {
-                    out.println("位置参数不正确");
+                    out.println("Wrong client location parameter");
                     out.flush();
                     out.close();
                 }
+            }else {
+                out.println("Client bad token ..");
+                out.flush();
+                out.close();
             }
         }
 
@@ -156,7 +166,7 @@ public class PushService extends HttpServlet {
                     PrintWriter pw;
                     pw = ctx.getResponse().getWriter();
 
-                    String site = new String("http://api.map.baidu.com/marker?location=30.739507,103.980957&title=我在这里&content=电子科技大学西区科技园&output=html&src=webapp.baidu.openAPIdemo");
+                    String site = new String("http://api.map.baidu.com/marker?location=30.739507,103.980957&title=ThisisName&content=This is Content Test&output=html&src=webapp.baidu.openAPIdemo");
                     byte[] temp = site.getBytes("UTF-8");
                     String siteUTF8 = new String(temp,"UTF-8");
 
